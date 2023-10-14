1572fa37f7e305545637de3c05d7bdcff73707ec
Owen Wendland <90295568+John-Biggums@users.noreply.github.com>
2023-10-14 16:42:26 -0700

    stand still when not pressing

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -48,33 +48,36 @@ public class Player extends Entity {
     }
 
     public void update(){
-        if(keyH.upPressed == true) {
+        if(keyH.upPressed == true || keyH.downPressed == true || keyH.rightPressed || keyH.leftPressed){
+            if(keyH.upPressed == true) {
             direction = "up";
             y -= speed;
-        }
-        else if(keyH.downPressed == true) {
-            direction = "down";
-            y += speed;
-        }
-        else if(keyH.rightPressed == true) {
-            direction = "right";
-            x += speed;
-        }
-        else if(keyH.leftPressed == true) {
-            direction = "left";
-            x -= speed;
+            }
+            else if(keyH.downPressed == true) {
+                direction = "down";
+                y += speed;
+            }
+            else if(keyH.rightPressed == true) {
+                direction = "right";
+                x += speed;
+            }
+            else if(keyH.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+            }
+
+            spriteCount++;
+            if(spriteCount > 14) {
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCount = 0;
+            }
         }
 
-        spriteCount++;
-        if(spriteCount > 10) {
-            if(spriteNum == 1){
-                spriteNum = 2;
-            }
-            else if(spriteNum == 2){
-                spriteNum = 1;
-            }
-            spriteCount = 0;
-        }
     }
 
     public void draw(Graphics2D g2){
