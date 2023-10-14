d67bd1da0ac4b3624d54b2399c936b1b6fe4fcb1
Owen Wendland <90295568+John-Biggums@users.noreply.github.com>
2023-10-14 16:38:57 -0700

    walking boy!!

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,5 @@
 package src.entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -65,6 +64,17 @@ public class Player extends Entity {
             direction = "left";
             x -= speed;
         }
+
+        spriteCount++;
+        if(spriteCount > 10) {
+            if(spriteNum == 1){
+                spriteNum = 2;
+            }
+            else if(spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCount = 0;
+        }
     }
 
     public void draw(Graphics2D g2){
@@ -75,18 +85,39 @@ public class Player extends Entity {
 
         switch(direction){
         case "up":
-            image = up1;
+            if (spriteNum == 1){
+                image = up1;
+            }
+            if (spriteNum == 2){
+                image = up2;
+            }
             break;
         case "down":
-            image = down1;
+            if (spriteNum == 1){
+                image = down1;
+            }
+            if (spriteNum == 2){
+                image = down2;
+            }
             break;
         case "left":
-            image = left1;
+            if (spriteNum == 1){
+                image = left1;
+            }
+            if (spriteNum == 2){
+                image = left2;
+            }
             break;
         case "right":
-            image = right1;
+            if (spriteNum == 1){
+                image = right1;
+            }
+            if (spriteNum == 2){
+                image = right2;
+            }
             break;
         }
+
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
